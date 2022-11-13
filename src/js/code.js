c97750dc5c467fea2b8e4b78d57d525824a1604a
Kevin Greer <kgrgreer@gmail.com>
2022-11-13 10:48:58 -0500

    Updated.

--- src/js/code.js
+++ src/js/code.js
@@ -1,15 +1,5 @@
 console.log('Start');
 
-var stack = [];
-var globals = {
-  print: function() {
-    console.log(stack.pop());
-  },
-  '+': function() {
-    stack.push(stack.pop(), stack.pop());
-  }
-};
-
 var input = `
 5
 4
@@ -19,25 +9,51 @@ print
 print
 `;
 
+var stack = [];
+var globals = {
+  read: (function() {
+    var lines = input.split('\n');
+    var ptr = 0;
+    return function() {
+      return ptr < lines.length ? lines[ptr++] : undefined;
+    };
+  })(),
+  eval: function(line) {
+    if ( line.startsWith('"') ) {
+      stack.push(line.substring(1));
+    } else if ( line == '' ) {
+    } else if ( line.charAt(0) >= '0' && line.charAt(0) <= '9' || line.charAt(0) == '-' ) {
+      stack.push(Number.parseInt(line));
+    } else {
+      var sym = globals[line];
+      if ( sym ) {
+        sym();
+      } else {
+        console.log('Unknown Symbol:', line);
+      }
+    }
+  },
+  Int: function(sym) {
+    return {
+      '+': function() {
+        stack.push(stack.pop(), stack.pop());
+      }
+    }[sym];
+  },
+  print: function() {
+    console.log(stack.pop());
+  },
+  '+': function() {
+    stack.push(stack.pop(), stack.pop());
+  }
+};
+
 var lines = input.split('\n');
 
-function eval(line) {
-  if ( line.startsWith('"') ) {
-    stack.push(line.substring(1));
-  } else if ( line == '' ) {
-  } else if ( line.charAt(0) >= '0' && line.charAt(0) <= '9' || line.charAt(0) == '-' ) {
-    stack.push(Number.parseInt(line));
-  } else {
-    var sym = globals[line];
-    if ( sym ) {
-      sym();
-    } else {
-      console.log('Unknown Symbol:', line);
-    }
-  }
-}
 
-for ( var i = 0 ; i < lines.length ; i++ ) {
-  var line = lines[i];
-  eval(line);
+var line;
+while ( true ) {
+  line = globals.read();
+  if ( line === undefined ) break;
+  globals.eval(line);
 }
