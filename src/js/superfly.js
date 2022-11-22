865f424df76db67d4d51bf76243c55589e6ae68d
Kevin Greer <kgrgreer@gmail.com>
2022-11-22 10:21:08 -0500

    eval now callable from scripts.

--- src/js/superfly.js
+++ src/js/superfly.js
@@ -1,22 +1,119 @@
-/*
-TODO:
-  - closures
-  - classes (as closures?)
-  - symbols
-  - symbol table
-  - stack frames
-  - function return values
-  - local variables (as functions?)
-  - constants?
-  - have eval take args from stack and be callable from scripts
-  - read() and readChar() should be callable from scripts
-*/
+var stack = [], sp /* stack pointer */;
+function isSpace(c) { return c === ' ' || c === '\t' || c === '\n' }
+var scope = {
+  debugger: function() { debugger; },
+  readChar: function() { return scope.ip < scope.input.length ? scope.input.charAt(scope.ip++) : undefined; },
+  read: function() {
+    var sym = '', c;
+    while ( c = this.readChar() ) {
+      if ( isSpace(c) ) {
+        if ( sym ) break;
+        continue;
+      }
+      sym += c;
+    }
+    return sym;
+  },
+  eval: function(code) {
+    var oldInput = scope.input, oldIp = scope.ip;
+    scope.input = code || stack.pop();
+    scope.ip    = 0;
+    var sym;
+    while ( sym = scope.read() ) {
+    //  console.log('---> ', sym);
+      var fn = scope.evalSym(sym);
+      fn && fn();
+    }
+    scope.input = oldInput;
+    scope.ip    = oldIp
+  },
+  evalSym: function(line) {
+    var sym = scope[line];
+    if ( sym ) return sym;
+    if ( line.startsWith(':') ) {
+      var sym   = line.substring(1);
+      var value = stack.pop();
+      return function() { scope[sym] = function() { stack.push(value); }; };
+    }
+    // TODO: define in language
+    if ( line === '//' ) {
+      while ( (c = scope.readChar()) != '\n' );
+      return;
+    }
+    // TODO: define in language
+    if ( line === '/*' ) {
+      while ( (c = scope.read()) != '*/' );
+      return;
+    }
+    if ( line === '"' ) {
+      var s = '', c;
+      while ( (c = scope.readChar()) != '"' ) s += c;
+      return function() { stack.push(s); };
+    }
+    if ( line.charAt(0) >= '0' && line.charAt(0) <= '9' || ( line.charAt(0) == '-' && line.length > 1 ) ) {
+      return function() { stack.push(Number.parseInt(line)); }
+    }
+    console.log('Unknown Symbol:', line);
+  },
+  print: function() { console.log(stack.pop()); },
+  '{': function() {
+    var l, oldScope = scope, vars = [], code = [];
 
-var input = `
+    scope = Object.create(scope);
+    // read var names
+    while ( ( l = scope.read() ) != '|' ) {
+      vars.push(l);
+    }
+
+    // define variable accessors
+    for ( var i = 0 ; i < vars.length ; i++ ) {
+      scope[vars[i]]       = function() { stack.push(stack[sp-vars.length+i]); };
+      scope[':' + vars[i]] = function() { stack[sp-vars.length+i] = stack.pop(); };
+    }
+
+    // read function body and add to code
+    while ( ( l = scope.read() ) != '}' ) {
+      code.push(scope.evalSym(l));
+    }
+
+    // create the function
+    stack.push(function() {
+      sp = stack.length-1;
+      for ( var i = 0 ; i < code.length ; i++ ) code[i]();
+    });
+
+    oldScope.ip = scope.ip;
+    scope = oldScope;
+  },
+  'not': function() { stack.push( ! stack.pop()); },
+  'and': function() { stack.push(stack.pop() &&  stack.pop()); },
+  'or':  function() { stack.push(stack.pop() ||  stack.pop()); },
+  '=':   function() { stack.push(stack.pop() === stack.pop()); },
+  '!=':  function() { stack.push(stack.pop() !== stack.pop()); },
+  '<':   function() { stack.push(stack.pop() >=  stack.pop()); },
+  '<=':  function() { stack.push(stack.pop() >   stack.pop()); },
+  '>':   function() { stack.push(stack.pop() <=  stack.pop()); },
+  '>=':  function() { stack.push(stack.pop() <   stack.pop()); },
+  '+':   function() { stack.push(stack.pop() +   stack.pop()); },
+  '*':   function() { stack.push(stack.pop() *   stack.pop()); },
+  '-':   function() { var a = stack.pop(), b = stack.pop(); stack.push(b - a); },
+  '/':   function() { var a = stack.pop(), b = stack.pop(); stack.push(b / a); },
+  '^':   function() { var a = stack.pop(), b = stack.pop(); stack.push(Math.pow(b,a)); },
+  'mod': function() { var a = stack.pop(), b = stack.pop(); stack.push(b % a); },
+  'if':  function() { var block = stack.pop(); var cond = stack.pop(); if ( cond ) block(); },
+  'ifelse': function() { var fBlock = stack.pop(), tBlock = stack.pop(), cond = stack.pop(); (cond ? tBlock : fBlock)(); },
+  '()':  function() { var fn = stack.pop(); fn(); }
+};
+
+// Language
+scope.eval(`
 1 1 = :true
 1 2 = :false
 { n | 0 n - } :neg // negate
+`);
 
+// Tests
+scope.eval(`
 // A comment
 " Starting..." print
 
@@ -27,6 +124,7 @@ var input = `
 4 2 * print
 4 2 / print
 10 3 mod print
+2 8 ^ print
 5 neg () print // it's inconsistent that some operators require () and others don't
 
 " Comparison Operators" print
@@ -69,7 +167,7 @@ helloWorld ()
 helloWorld callFiveTimes ()
 
 " Conditionals" print
-true { | " is true" print } if
+true  { | " is true"  print } if
 false { | " is false" print } if
 
 true  { | " if true" print } { | " if false" print } ifelse
@@ -86,6 +184,8 @@ false { | " if true" print } { | " if false" print } ifelse
   i 1 + :i i print
 } ()
 
+" 1 + 2 print" eval
+
 /*
  TODO: fix, needs closure support to work
 { start end block |
@@ -99,101 +199,20 @@ false { | " if true" print } { | " if false" print } ifelse
 */
 
 " Done." print
-`;
+`);
 
-var stack = [], sp /* stack pointer */, ip = 0 /* input pointer */;
-function isSpace(c) { return c === ' ' || c === '\t' || c === '\n' }
-var scope = {
-  debugger: function() { debugger; },
-  readChar: function() { return ip < input.length ? input.charAt(ip++) : undefined; },
-  read: function() {
-    var sym = '', c;
-    while ( c = this.readChar() ) {
-      if ( isSpace(c) ) {
-        if ( sym ) break;
-        continue;
-      }
-      sym += c;
-    }
-    return sym;
-  },
-  eval: function(line) {
-    var sym = scope[line];
-    if ( sym ) return sym;
-    if ( line.startsWith(':') ) {
-      var sym   = line.substring(1);
-      var value = stack.pop();
-      return function() { scope[sym] = function() { stack.push(value); }; };
-    }
-    // TODO: define in language
-    if ( line === '//' ) {
-      while ( (c = scope.readChar()) != '\n' );
-      return;
-    }
-    // TODO: define in language
-    if ( line === '/*' ) {
-      while ( (c = scope.read()) != '*/' );
-      return;
-    }
-    if ( line === '"' ) {
-      var s = '', c;
-      while ( (c = scope.readChar()) != '"' ) s += c;
-      return function() { stack.push(s); };
-    }
-    if ( line.charAt(0) >= '0' && line.charAt(0) <= '9' || ( line.charAt(0) == '-' && line.length > 1 ) ) {
-      return function() { stack.push(Number.parseInt(line)); }
-    }
-    console.log('Unknown Symbol:', line);
-  },
-  print: function() { console.log(stack.pop()); },
-  '{': function() {
-    var l, oldGlobal = scope, vars = [], code = [];
 
-    scope = Object.create(scope);
 
-    // read var names
-    while ( ( l = scope.read() ) != '|' ) vars.push(l);
-
-    // define variable accessors
-    for ( var i = 0 ; i < vars.length ; i++ ) {
-      scope[vars[i]]       = function() { stack.push(stack[sp-vars.length+i]); };
-      scope[':' + vars[i]] = function() { stack[sp-vars.length+i] = stack.pop(); };
-    }
-
-    // read function body and add to code
-    while ( ( l = scope.read() ) != '}' ) code.push(scope.eval(l));
-
-    // create the function
-    stack.push(function() {
-      sp = stack.length-1;
-      for ( var i = 0 ; i < code.length ; i++ ) code[i]();
-    });
-
-    scope = oldGlobal;
-  },
-  'not': function() { stack.push( ! stack.pop()); },
-  'and': function() { stack.push(stack.pop() &&  stack.pop()); },
-  'or':  function() { stack.push(stack.pop() ||  stack.pop()); },
-  '=':   function() { stack.push(stack.pop() === stack.pop()); },
-  '!=':  function() { stack.push(stack.pop() !== stack.pop()); },
-  '<':   function() { stack.push(stack.pop() >=  stack.pop()); },
-  '<=':  function() { stack.push(stack.pop() >   stack.pop()); },
-  '>':   function() { stack.push(stack.pop() <=  stack.pop()); },
-  '>=':  function() { stack.push(stack.pop() <   stack.pop()); },
-  '+':   function() { stack.push(stack.pop() +   stack.pop()); },
-  '*':   function() { stack.push(stack.pop() *   stack.pop()); },
-  '-':   function() { var a = stack.pop(), b = stack.pop(); stack.push(b - a); },
-  '/':   function() { var a = stack.pop(), b = stack.pop(); stack.push(b / a); },
-  'mod': function() { var a = stack.pop(), b = stack.pop(); stack.push(b % a); },
-  'if':  function() { var block = stack.pop(); var cond = stack.pop(); if ( cond ) block(); },
-  'ifelse': function() { var fBlock = stack.pop(), tBlock = stack.pop(), cond = stack.pop(); (cond ? tBlock : fBlock)(); },
-  '()':  function() { var fn = stack.pop(); fn(); }
-};
-
-// TODO: move this to a scope function which takes value to parse from stack
-var sym;
-while ( sym = scope.read() ) {
-//  console.log('---> ', sym);
-  var fn = scope.eval(sym);
-  fn && fn();
-}
+/*
+TODO:
+  - closures
+  - classes (as closures?)
+  - symbols
+  - symbol table
+  - stack frames
+  - function return values
+  - local variables (as functions?)
+  - constants?
+  - have eval take args from stack and be callable from scripts
+  - read() and readChar() should be callable from scripts
+*/
